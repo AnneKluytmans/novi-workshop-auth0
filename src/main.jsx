0765ffb5c3113419f9f9c68a20c57b8e6a8dc4ae
camielvilla <camielvilla@gmail.com>
2026-06-22 09:20:46 +0200

    update

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.render(
       <AuthContextProvider>
         <App/>
       </AuthContextProvider>
-    </Router>KAPOT
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
