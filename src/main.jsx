dec9be00f7111adcd8d382c6f1510b26fbf867a8
AnneKluytmans <annekluytmans1@gmail.com>
2026-06-24 12:53:10 +0200

    feat: wrap App in main.jsx with Auth0Provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import AuthContextProvider from './context/AuthContext';
 import App from './App';
 import './index.css';
+import {Auth0Provider} from "@auth0/auth0-react";
+
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <AuthContextProvider>
+      <Auth0Provider
+        domain={import.meta.env.VITE_AUTH0_DOMAIN}
+        clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+        authorizationParams={{
+            // redirect_uri: is de pagina waarnaar Auth0 terugkeert na inloggen — meestal de homepage, hier de profielpagina.
+            redirect_uri: window.location.origin/profile,
+            audience: import.meta.env.VITE_AUTH0_AUDIENCE,
+        }}
+      >
         <App/>
-      </AuthContextProvider>
+      </Auth0Provider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
